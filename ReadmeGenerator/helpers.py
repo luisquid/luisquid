3c4423d7898419e22e8d566dda0f0fc9eb52c65b
Squid <luis.bazan@unity3d.com>
2025-04-25 12:16:24 +0100

    Fix image pls

--- ReadmeGenerator/helpers.py
+++ ReadmeGenerator/helpers.py
@@ -44,8 +44,9 @@ def right_image(data, context):
 
 def tech_stack(data, context):
     title = process_title(data["title"], context)
+    right_image = right_image(data["right_image"])
     tech = "- " + "\n- ".join(data["tech"])
-    return f"{title}\n{tech}\n"
+    return f"{title}\n{right_image}\n{tech}\n"
 
 
 def awesome_projects(data, context):
